6735336eb7f33d56a1ceff159a0acf83b9ec9f2a
René Rask <renerask@gmail.com>
2016-08-04 21:25:07 +0200

    Add ansible setup

---
+++ ansible/files/user.js
@@ -0,0 +1,6 @@
+user_pref('browser.startup.homepage', 'http://localhost');
+user_pref('browser.cache.disk.enable', false);
+user_pref('browser.cache.memory.enable', true);
+user_pref('browser.cache.memory.capacity', 50000);
+user_pref("browser.sessionstore.resume_from_crash", false);
+
